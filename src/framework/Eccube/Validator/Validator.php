5d47a4adf325df45ce9067fa73a6c7aec77c0f6a
kiy0taka <kiy0taka333@gmail.com>
2023-04-26 14:06:08 +0900

    Validator追加

---
+++ src/framework/Eccube/Validator/Validator.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) EC-CUBE CO.,LTD. All Rights Reserved.
+ *
+ * http://www.ec-cube.co.jp/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eccube\Validator;
+
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class Validator
+{
+    private ValidatorInterface $validator;
+
+    public function __construct(ValidatorInterface $validator)
+    {
+        $this->validator = $validator;
+    }
+
+    /**
+     * @param $value
+     * @param Constraint|Constraint[] $constants
+     * @return ConstraintViolationList
+     */
+    public function validate($value, $constants): ConstraintViolationList
+    {
+        return new ConstraintViolationList($this->validator->validate($value, $constants));
+    }
+}
