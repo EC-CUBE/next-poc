6b5315adbe2e7631f7275a096ae758dee0390d0a
kiy0taka <kiy0taka333@gmail.com>
2023-06-22 14:31:53 +0900

    Symfony\Component\HttpFoundation\Cookie -> Eccube\Http\Cookie

--- src/application/Eccube/Service/TwoFactorAuthService.php
+++ src/application/Eccube/Service/TwoFactorAuthService.php
@@ -17,7 +17,7 @@ use Eccube\Common\EccubeConfig;
 use Eccube\Security\Core\User\UserPasswordHasher;
 use RobThree\Auth\TwoFactorAuth;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpFoundation\Cookie;
+use Eccube\Http\Cookie;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 
