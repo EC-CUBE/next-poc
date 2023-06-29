dd831a94d7809c737e83dd117d51557b9183fc5f
kiy0taka <kiy0taka333@gmail.com>
2023-06-29 13:54:10 +0900

    EventDispatcher

---
+++ src/framework/Eccube/EventDispatcher/EventDispatcherDecorator.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) EC-CUBE CO.,LTD. All Rights Reserved.
+ *
+ * http://www.ec-cube.co.jp/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eccube\EventDispatcher;
+
+use Psr\EventDispatcher\StoppableEventInterface;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class EventDispatcherDecorator extends EventDispatcher
+{
+    protected function callListeners(iterable $listeners, string $eventName, object $event)
+    {
+        $stoppable = $event instanceof StoppableEventInterface;
+
+        foreach ($listeners as $listener) {
+            if ($stoppable && $event->isPropagationStopped()) {
+                break;
+            }
+            if (is_array($listener) && $listener[0] instanceof EventSubscriberInterface) {
+                $listener(new Event($event), $eventName, $this);
+                continue;
+            }
+            $listener($event, $eventName, $this);
+        }
+    }
+
+}
